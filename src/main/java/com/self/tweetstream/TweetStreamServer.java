342c193fa6b6100cd9ece215e07b86e89679d81f
harith <harith@yahoo-inc.com>
2014-07-19 15:41:05 -0700

    works correctly with multiple tabs and multiple browsers

--- src/main/java/com/self/tweetstream/TweetStreamServer.java
+++ src/main/java/com/self/tweetstream/TweetStreamServer.java
@@ -1,10 +1,14 @@
 package com.self.tweetstream;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
@@ -16,34 +20,40 @@ import com.twitter.hbc.core.Client;
 @ServerEndpoint("/tweets")
 public class TweetStreamServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TweetStreamServer.class);
+    private static final ConcurrentHashMap<Session, Object> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void open(Session session) {
+        LOGGER.debug("new session {}", session.getId());
+        sessions.put(session, new Object());
+    }
 
     @OnMessage
     public void tweets(final String message, final Session session) throws IOException, InterruptedException {
         System.out.println("session id:" + session.getId() + ", search term: " + message);
+        sessions.put(session, new Object());
         final Client twitterClient = TwitterHoseBird.getInstance(message);
         while (!session.getOpenSessions().isEmpty()) {
             for (final Session s : session.getOpenSessions()) {
                 if (twitterClient.isDone()) {
+                    TwitterHoseBird.getMsgQueue().poll(2, TimeUnit.SECONDS);
                     System.out.println("Twitter Client Done, waiting ...");
                 }
-                s.getBasicRemote().sendText(TwitterHoseBird.getMsgQueue().take());
+                s.getAsyncRemote().sendText(TwitterHoseBird.getMsgQueue().take());
             }
         }
     }
-/*
-    @OnMessage
-    public void tweets(final String message, final Session client) throws IOException, InterruptedException {
-        System.out.println("session id:" + client.getId() + ", search term: " + message);
-        final TwitterHoseBird twitterHoseBird = new TwitterHoseBird();
-        final Client twitterClient = twitterHoseBird.getInstance(message);
-        while (!twitterClient.isDone()) {
-            client.getBasicRemote().sendText(twitterHoseBird.getMsgQueue().take());
-        }
+
+    @OnError
+    public void onError(Throwable t, Session session) {
+        LOGGER.warn("error:{}", t.getMessage());
+        sessions.remove(session);
+        LOGGER.debug("closing session {}", session);
     }
-*/
 
     @OnClose
     public void onClose(Session session, CloseReason reason) throws IOException {
+        sessions.remove(session);
         LOGGER.warn("closing session: {}, remaining session: {}", session.getId(), session.getOpenSessions().size());
     }
 }
