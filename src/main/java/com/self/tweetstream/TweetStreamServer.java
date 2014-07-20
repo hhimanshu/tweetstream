48a6bc20ea0a4fbedaf486b331ab23fdbea0acf9
harith <harith@yahoo-inc.com>
2014-07-19 21:14:44 -0700

    Simple Page showing Google Maps API with a static marker

--- src/main/java/com/self/tweetstream/TweetStreamServer.java
+++ src/main/java/com/self/tweetstream/TweetStreamServer.java
@@ -1,8 +1,6 @@
 package com.self.tweetstream;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import javax.websocket.CloseReason;
@@ -21,18 +19,15 @@ import com.twitter.hbc.core.Client;
 @ServerEndpoint("/tweets")
 public class TweetStreamServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TweetStreamServer.class);
-    private static final Map<Session, Object> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
     public void open(final Session session) {
         LOGGER.debug("new session {}", session.getId());
-        sessions.put(session, new Object());
     }
 
     @OnMessage
     public void tweets(final String message, final Session session) throws IOException, InterruptedException {
         LOGGER.debug("new session id: {}", session.getId());
-        sessions.put(session, new Object());
 
         final Client twitterClient = TwitterHoseBird.getInstance();
         while (!session.getOpenSessions().isEmpty()) {
@@ -49,13 +44,11 @@ public class TweetStreamServer {
     @OnError
     public void onError(final Throwable t, final Session session) {
         LOGGER.warn("error:{}", t.getMessage());
-        sessions.remove(session);
         LOGGER.debug("closing session {}", session);
     }
 
     @OnClose
     public void onClose(final Session session, final CloseReason reason) throws IOException {
-        sessions.remove(session);
         LOGGER.warn("closing session: {}, remaining session: {}", session.getId(), session.getOpenSessions().size());
     }
 }
